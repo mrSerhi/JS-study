9fbd78a1d12b0c967506da7f102edb7760e5b7b8
Serhii Marushchenko <serhii.mar90@gmail.com>
2018-12-14 22:59:37 +0200

    if and else statements

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+var userName = prompt('What is your name?', 'Serhii');
+if (userName === '' || !isNaN(userName) || userName.length < 4) {
+    alert('Entered data wrong!');
+} else {
+    var userAge = +prompt("What is your age?", 28);
+    if (isNaN(userAge) || null && undefined || userAge < 18) {
+        alert('Goodbye darling, you chooses not a good way...');
+        false;
+    } else {
+        console.log('Okey, you are 18 years old. All right. Welcome ' + userName)
+    }
+}
+
