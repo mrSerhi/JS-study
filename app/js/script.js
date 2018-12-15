e4978a1f0e0730f073a2154426a95518c7880a23
Serhii Marushchenko <serhii.mar90@gmail.com>
2018-12-15 14:47:47 +0200

    Add the small game with if else statments between user and computer

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+/*
 var userName = prompt('What is your name?', 'Serhii');
 if (userName === '' || !isNaN(userName) || userName.length < 4) {
     alert('Entered data wrong!');
@@ -10,4 +11,38 @@ if (userName === '' || !isNaN(userName) || userName.length < 4) {
         console.log('Okey, you are 18 years old. All right. Welcome ' + userName)
     }
 }
+*/
 
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// var randNumber = getRandomArbitrary(1, 40);
+var userNumber = +prompt('Write a number, no higher than 20', 20);
+var computer = 0;
+var winnerPoints = 0;
+
+function game(userNumber) {
+    if (!userNumber || isNaN((userNumber)) || userNumber === '' || null && undefined) {
+        alert('This is not a number, or doesn\'t have anithing into self... Try again');
+    } else if (userNumber > 20) {
+        alert('Hey...We have agreed that number will be not bigger than 20!');
+    } else {
+        computer += getRandomArbitrary(1, 30);
+        if (userNumber > computer) {
+            winnerPoints += userNumber - computer;
+            alert("Congratulations! You win with " + winnerPoints + ' points');
+        } else if (userNumber < computer) {
+            // winnerPoints+=computer - userNumber;
+            // alert("Ohhh no...You lose =( and the computer has more on  " + winnerPoints + ' points');
+            alert('Ohhh no...You lose =( and the computer is Win');
+        } else {
+            alert('It seems you and the computer have equel points');
+            console.log('Computer has - ' + computer + ' points');
+            console.log('You have - ' + userNumber + ' points');
+        }
+    }
+}
+
+game(userNumber);
