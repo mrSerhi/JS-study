8dbfc9498bed921903b1dc35b490ec372e6cdf89
Serhii Marushchenko <serhii.mar90@gmail.com>
2018-12-18 16:34:56 +0200

    Start plaing with loops

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('Hello world!');
+console.log('Hello Loops!');
+
+var users = ['Ani', 'Bob', 'Tim', 'Robin', 'Djeck'];
+var localCopyUsers = users.slice();
+for(var i = 0; i < users.length; i++) {
+    localCopyUsers[i]+= ' user #' + i;
+    console.log(localCopyUsers[i]);
+    // console.log(localCopyUsers);
+}
+console.log(localCopyUsers);
+
+for(var j = users.length -1; j >= 0; j--) {
+    console.log(users[j]);
+}
+
+var num = [10, 20, 30, 40, 50, 60];
+for(var d = 0; d < num.length; d++) {
+    if (num[d] === 50) {
+        break;
+    } else if (num[d] === 20) {
+        continue;
+    }
+    console.log(num[d]);
+}
+
+var i = 0;
+var arr = [];
+while(i < 10) {
+    var object = new Object();
+    arr.push(object);
+    i++;
+}
+console.log(arr);
