5adb736051a46c24c04f3f9d827ef4e006a96cac
Serhii Marushchenko <serhii.mar90@gmail.com>
2018-12-18 17:31:35 +0200

    Was added small example with loops+arrays+function which create sort and write in another array  users age

--- app/js/script.js
+++ app/js/script.js
@@ -25,10 +25,36 @@ for(var d = 0; d < num.length; d++) {
 }
 
 var i = 0;
-var arr = [];
+var arr = []; // 10 - {}
 while(i < 10) {
     var object = new Object();
     arr.push(object);
     i++;
 }
 console.log(arr);
+
+var userYearsOfBorn = [1990, 1975, 2017, 2016, 2018, 1988, 2000, 2004, 1955, 2010, 2012, 2008];
+var age = [];
+var trueAge = [];
+var falseAge = [];
+for (var i = 0; i < userYearsOfBorn.length; i++) {
+    var dateNow = new Date();
+    age.push( dateNow.getFullYear() - userYearsOfBorn[i] );
+}
+// console.log(trueAge);
+for (var j = 0; j < age.length; j++) {
+    if (age[j] >= 18) {
+        console.log('User №' + j + ' have ' + age[j] + ' age');
+        checkUserAge(age[j]);
+    } else {
+        // continue;
+        checkUserAge(age[j]);
+    }
+}
+
+function checkUserAge(item) {
+    return item >= 18 ? trueAge.push(item) : falseAge.push(item);
+}
+
+console.log(trueAge);
+console.log(falseAge);
