c772a676849d1c3164c599501c6532034ee96ad1
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-01-08 19:53:50 +0200

    What is it context and where that lives

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,192 @@
 "use strict";
-console.log('Hello world!');
-console.log(parseFloat(0.99 + 0.95 + '$').toFixed(2));
 
-function char(str) {
-    return str ? str[0].toUpperCase() + str.slice(1) : '';
+var valia = {
+    name: 'Valia'
+};
+
+var kirill = {
+    name: 'kirill'
+};
+
+valia.hello = sayHi;
+kirill.hello = sayHi;
+
+valia.hello();
+kirill.hello();
+
+function sayHi() {
+    return 'Hello ' + this.name;
 }
-var str = 'Vasia';
-var secondStr = char('vasia');
-var empty = char();
-//console.log(str === secondStr);
-//console.log(empty === '');
+// ===============================================
 
-function checkSpam(str) {
-    var lowerStr = str.toLowerCase();
-    if (lowerStr.indexOf('viagra', 0) !== -1 || lowerStr.indexOf('xxx', 0) !== -1) {
-        return true;
+var lolo = ['a', 'b'];
+
+// lolo.push(function() {console.log(this)});
+
+// lolo[2](); // context in to array
+
+var userName = 'Huy Sobachiy';
+
+var hey = {
+    userName: 'Glod',
+    export: function () {
+        return this
+    },
+    lol: this // refer on to global scope (window)
+};
+
+hey.lol.userName; // Huy Sobachiy
+
+var babila = {
+    name: 'Petia',
+    export: function () {
+        return {
+            value: this
+        };
     }
-    return false;
-}
-var spamOne = 'To buy Viagra and watch tv XXxxx';
-var xxx = 'watch XXX video';
-var good = 'hey you, go with us and nave a fun!'
-console.log(checkSpam(good));
-// console.log(spamOne.indexOf('viagra'))
+};
 
-var hey = 'Now is time for our teem bealding and cooperation with big boss';
-var bobo = 'this is terrible how do you do at this?';
-var hoHo = 'How do you feel?';
-function truncate(str, maxlength) {
-    var diff;
+var kolia = {
+    name: 'Kolia'
+};
 
-    if (str.length > maxlength) {
-        // diff = (maxlength - str.length) - 3;
-        return str.slice(0, maxlength - 3) + '...';
+kolia.export = babila.export;
+
+
+kolia.export().value.name; // kolia
+babila.export().value.name; // petia
+// =================================================
+
+var calc = {
+    read: function () {
+        this.numbers = []
+        for (let i = 0; i < 2; i++) {
+            var prom = prompt('Number #1', 5);
+            this.numbers.push(parseInt(prom));
+        }
+
+        return this.numbers;
+    },
+    sum: function (a, b) {
+        return this.numbers.reduce(function (acc, cur) {
+            return acc + cur;
+        });
+    },
+    mul: function () {
+        return this.numbers.reduce(function (acc, cur) {
+            return acc * cur;
+        });
     }
-    return str;
+
+};
+// calc.read();
+// console.log(calc.sum());
+// console.log(calc.mul());
+// ===================================================
+
+var ladder = {
+    step: 0,
+    up: function () { // вверх по лестнице
+        this.step++;
+        return this;
+    },
+    down: function () { // вниз по лестнице
+        this.step--;
+        return this;
+    },
+    showStep: function () { // вывести текущую ступеньку
+        alert(this.step);
+    }
+};
+// ladder.up().up().up().down().showStep();
+// ========================================================
+
+function Calculator() {
+    this.answers = [];
 }
-console.log(truncate(hey, 20));
-console.log(truncate(hoHo, 20));
-console.log(truncate(bobo, 20));
+
+Calculator.prototype.read = function () {
+    for (let i = 0; i < 2; i++) {
+        var quest = +prompt('Number is', 5);
+        this.answers.push(quest);
+    }
+}
+
+Calculator.prototype.sum = function () {
+    return this.answers[0] + this.answers[1];
+}
+
+Calculator.prototype.mul = function () {
+    return this.answers[0] * this.answers[1];
+}
+
+var culc = new Calculator();
+// culc.read();
+// console.log(culc.sum());
+// console.log(culc.mul());
+// =================================================
+
+function Acc(num) {
+    this.value = num;
+    this.read = function () {
+        return this.value += parseInt(prompt('Number', 5));
+    };
+}
+
+var accumo = new Acc(1);
+/*
+accumo.read();
+accumo.read();
+console.log(accumo.value);
+*/
+// =======================================================
+
+function Fun() {
+
+    var methods = {
+        "+": function (a, b) {
+            return a + b;
+        },
+        "-": function (a, b) {
+            return a - b;
+        }
+    };
+
+    this.calculate = function (str) {
+        var splitStr, numF, numS, operator;
+
+        splitStr = str.split(' ');
+        numF = +splitStr[0];
+        operator = splitStr[1];
+        numS = +splitStr[2];
+
+        return methods[operator](numF, numS);
+    };
+
+    this.addMethod = function (name, func) {
+        if (!methods.name) {
+            methods[name] = func;
+        } else {
+            console.warn('Method used!');
+            return false;
+        }
+    };
+}
+
+var sum = new Fun();
+sum.addMethod('**', function (a, b) {
+    return Math.pow(a, b);
+});
+sum.addMethod('/', function (a, b) {
+    return a / b;
+});
+sum.addMethod('/', function (a, b) {
+    return a / b;
+});
+sum.addMethod('+', function (a, b) {
+    return a + b;
+});
+
+// console.log(sum.calculate('2 + 2'));
+// console.log(sum.calculate('2 ** 3'));
+// console.log(sum.calculate('21 / 3'));
