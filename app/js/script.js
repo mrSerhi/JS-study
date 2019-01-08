34a147184dc49cc6937debfcb4cb1d0dfcf99c49
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-01-08 10:50:21 +0200

    make some examples with closures

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,218 @@
 "use strict";
-console.log('Hello world!');
-console.log(parseFloat(0.99 + 0.95 + '$').toFixed(2));
 
-function char(str) {
-    return str ? str[0].toUpperCase() + str.slice(1) : '';
-}
-var str = 'Vasia';
-var secondStr = char('vasia');
-var empty = char();
-//console.log(str === secondStr);
-//console.log(empty === '');
+function counter() {
+    var count = 1;
 
-function checkSpam(str) {
-    var lowerStr = str.toLowerCase();
-    if (lowerStr.indexOf('viagra', 0) !== -1 || lowerStr.indexOf('xxx', 0) !== -1) {
-        return true;
+    return function () {
+        return count++;
     }
-    return false;
 }
-var spamOne = 'To buy Viagra and watch tv XXxxx';
-var xxx = 'watch XXX video';
-var good = 'hey you, go with us and nave a fun!'
-console.log(checkSpam(good));
-// console.log(spamOne.indexOf('viagra'))
 
-var hey = 'Now is time for our teem bealding and cooperation with big boss';
-var bobo = 'this is terrible how do you do at this?';
-var hoHo = 'How do you feel?';
-function truncate(str, maxlength) {
-    var diff;
+var count = counter();
+count();
+count();
+count();
+count();
 
-    if (str.length > maxlength) {
-        // diff = (maxlength - str.length) - 3;
-        return str.slice(0, maxlength - 3) + '...';
+var anotherCount = counter();
+anotherCount();
+anotherCount();
+anotherCount();
+anotherCount();
+
+// =========================================================
+
+var nameVasia = createString(name, 45);
+
+var userName = 'Vasia';
+
+function createString(name, age) {
+    var userName = name;
+    var userAge = age;
+    var greet = '';
+
+    return function () {
+        return greet += 'hello ' + userName + '. You have a ' + userAge + ' age.'
+    };
+}
+
+// var nameVasia = createString('Vasia', 45);
+
+// console.log(nameVasia());
+
+// ============================================================
+howHow(5555); // false, numberHujamber === undefined
+
+var numberHujamber = 5555;
+
+howHow(5555); // true
+
+function howHow(pass) {
+
+    return pass === numberHujamber ? true : false;
+}
+
+// ==============================================================
+
+var value = 0;
+
+function fun() {
+    // var value; on stage init, variable will be created
+    if (1) {
+        value = true;
+    } else {
+        var value = false;
     }
-    return str;
+
+    alert(value);
 }
-console.log(truncate(hey, 20));
-console.log(truncate(hoHo, 20));
-console.log(truncate(bobo, 20));
+
+// fun();
+// console.log( value ); // global value not change!
+// ==============================================================
+
+function test() {
+
+    alert(window); // undefined
+
+    var window = 5;
+
+    alert(window); // 5
+}
+
+//   test();
+
+// =============================================================
+
+var twelve = sum(6)(6);
+
+function sum(a) {
+    return function (b) {
+        return a + b;
+    }
+}
+// ==============================================
+
+var buffer = makeBuffer();
+// buffer(0);
+// buffer(1);
+// buffer(0);
+
+// buffer();
+// buffer.clear();
+// buffer();
+
+
+function makeBuffer() {
+    var acc = '';
+
+    function buff(val) {
+        return arguments.length !== 0 ? acc += ' ' + val : console.log(acc);
+    }
+
+    buff.clear = function () {
+        acc = '';
+    };
+
+    return buff;
+}
+// =================================================================
+
+var users = [{
+    name: "Вася",
+    surname: 'Иванов',
+    age: 20
+}, {
+    name: "Петя",
+    surname: 'Чапаев',
+    age: 25
+}, {
+    name: "Маша",
+    surname: 'Медведева',
+    age: 18
+}];
+
+/*
+users.sort(byField('name'));
+users.forEach(function (user) {
+    console.log(user.name);
+});
+
+console.log('------------------');
+
+users.sort(byField('age'));
+users.forEach(function (user) {
+    console.log(user.name);
+});
+*/
+
+
+function byField(field) {
+
+    return function (a, b) {
+        return a[field] > b[field] ? 1 : -1;
+    }
+}
+
+// ========================================================
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+var even = filter(arr, function (a) {
+    return a % 2 === 0;
+}); // [ 2, 4, 6 ]
+
+var rang = filter(arr, inBetween(3, 6));
+var areo = filter(arr, inArray([2, 4, 10]));
+
+function inBetween(a, b) {
+    return function (num) {
+        return num >= a && num <= b;
+    };
+}
+
+// function inArray(arr) {
+//     return function(num) {
+//         return arr.indexOf(num) !== -1;
+//     };
+// }
+
+function inArray(arr) {
+    return function (x) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return true;
+            }
+        }
+    }
+}
+
+function filter(arr, func) {
+    var res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            res.push(arr[i]);
+        }
+    }
+
+    return res;
+};
+// ===========================================================
+
+function makeArmy() {
+
+    var shooters = [];
+
+        for (var i = 0; i < 10; i++) {
+             function shooter() { // функция-стрелок
+                console.log(i); // выводит свой номер
+            }
+            shooters.push(shooter);
+        }
+    return shooters;
+}
+
+var army = makeArmy();
+
+army[0]();
