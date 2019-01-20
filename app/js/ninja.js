a48fef82afab53b4cd355c80272a9cb8c7117e94
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-01-20 19:40:41 +0200

    polymorphism, mixins, composition and execise

--- app/js/ninja.js
+++ app/js/ninja.js
@@ -47,7 +47,7 @@ function Person(name) {
     console.log(`Hello ${this.name}`);
   };
 }
-const Animal = new Object();
+// const Animal = new Object();
 const Human = {};
 
 const Func = new Function("gender", `this.gender = gender`);
@@ -214,3 +214,201 @@ Object.defineProperty(ss, "name", {
   enumerable: false,
   configurable: false
 });
+// =============================================================================
+function extend(child, parent) {
+  child.prototype = Object.create(parent.prototype);
+  child.prototype.constructor = child;
+}
+
+function Star(name = "", weight = null, radius = null, number = null) {
+  this.name = name;
+  this.weight = weight;
+  this.radius = radius;
+  this.number = number;
+}
+Star.prototype.getDistanceToSatellite = function() {
+  return `distance to satellite is very big`;
+};
+
+function Planet(name, weight, radius, number, mass, traectory = "elipce") {
+  // call super constructor
+  Star.call(this, name, weight, radius, number);
+
+  this.mass = mass;
+  this.traectory = traectory;
+}
+
+extend(Planet, Star);
+
+Planet.prototype.sayHi = function() {
+  return `Hello EvryOne, I'm the ${this.name} planet`;
+};
+
+Planet.prototype.getDistanceToSatellite = function() {
+  return `Distance to satellite is the fucking big number, dudde!`;
+};
+
+function Satellite(name, weight, radius, year, state = true) {
+  Star.call(this, name, weight, radius);
+
+  this.yearOfBirn = year;
+  this.state = state;
+}
+
+extend(Satellite, Star);
+
+Satellite.prototype.getDistanceToSatellite = function() {
+  return `I'm the Satellite, and my distance non exist =)`;
+};
+
+const moon = new Planet("Moon", 3500, 8900, 344, 120340);
+const sun = new Star();
+const niero = new Satellite("Niero", 11000, 4000, 1997);
+
+const objects = [moon, sun, niero];
+// displayDistance(objects);
+
+function displayDistance(objects) {
+  for (const obj of objects) {
+    console.log(obj.getDistanceToSatellite());
+  }
+}
+
+// =================================================================
+
+// mixins and composition
+
+// mixins
+const walking = {
+  canWalc: function() {
+    return `Animal can walc`;
+  }
+};
+
+const eating = {
+  canEat: function() {
+    return `Animal can eat`;
+  }
+};
+
+const flying = {
+  canFly: function() {
+    return `Animal can fly`;
+  }
+};
+
+const swiming = {
+  canSwim: function() {
+    return `Animal can swim`;
+  }
+};
+
+function mixin(target, ...sources) {
+  Object.assign(target.prototype, ...sources);
+}
+
+function Animal(name, age, habitat) {
+  this.name = name;
+  this.age = age;
+  this.habitat = habitat;
+}
+
+Animal.prototype.displaMessage = function() {
+  return `This animal is ${this.name} has ${this.age} years and dwells in ${
+    this.habitat
+  } habitat`;
+};
+
+function Wolf(name, age, habitat) {
+  Animal.call(this, name, age, habitat);
+}
+
+extend(Wolf, Animal);
+mixin(Wolf, eating, walking);
+
+function Bird(name, age, habitat) {
+  Animal.call(this, name, age, habitat);
+}
+
+extend(Bird, Animal);
+mixin(Bird, flying, eating);
+
+// Object.assign(Bird.prototype, fly);
+
+function Fish() {
+  Animal.apply(this, arguments);
+}
+
+extend(Fish, Animal);
+mixin(Fish, eating, swiming);
+
+const eagle = new Bird("Eagle", 3, "sky");
+const wolf = new Wolf("Wolf", 9, "ground");
+const whale = new Fish("Whale", 34, "ocean");
+
+// =============================================================
+
+function HtmlElement() {
+  this.click = function() {
+    console.log("clicked");
+  };
+}
+
+HtmlElement.prototype.focus = function() {
+  console.log("Focused");
+};
+
+function HtmlSelectElement(arr = []) {
+  // HtmlElement.call(this);
+
+  this.items = arr;
+  this.addItem = function(...item) {
+    this.items.push(...item);
+  };
+  this.removeItem = function(item) {
+    if (this.items.includes(item))
+      this.items.splice(this.items.indexOf(item), 1);
+  };
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+// HtmlSelectElement.prototype = Object.create(HtmlElement.prototype);
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+// HtmlSelectElement.prototype.click = function() {
+//   HtmlElement.call(this);
+//   this.click();
+// };
+HtmlSelectElement.prototype.render = function() {
+  return `
+<select>
+${this.items.map(el => `<option>${el}</option>`).join('')}
+</select>
+`;
+};
+
+function HtmlImageElement(src = "") {
+  this.src = src;
+  this.render = function() {
+    return `<img src="${this.src}" />`;
+  };
+}
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const img = new HtmlImageElement();
+const button = new HtmlElement();
+const htmlElements = new HtmlSelectElement();
+htmlElements.addItem("title", "value", "src", "href");
+htmlElements.removeItem("src");
+// htmlElements.click()
+// htmlElements.focus();
+img.src = "http://google.com";
+const elements = [img, htmlElements];
+// dipsplayRender();
+
+function dipsplayRender() {
+  for (const elem of elements) {
+    console.log(elem.render());
+  }  
+}
+// ===========================================================================
