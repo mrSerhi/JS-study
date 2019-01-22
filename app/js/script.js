8fb5e832156582f5c7a68182266d3a64705ee39b
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-01-22 17:09:47 +0200

    promises instead callback hell

--- app/js/script.js
+++ app/js/script.js
@@ -1,112 +1,116 @@
-"use strict";
+{
+  ("use strict");
 
-function sayHi() {
-  setTimeout(() => {
-    console.log("Async");
-  }, 2000);
-}
+  function sayHi() {
+    setTimeout(() => {
+      console.log("Async");
+    }, 2000);
+  }
 
-function init() {
-  //   console.log("Init function");
-  setTimeout(() => {
-    console.log("Init function");
-  }, 1000);
-}
+  function init() {
+    //   console.log("Init function");
+    setTimeout(() => {
+      console.log("Init function");
+    }, 1000);
+  }
 
-function doIt() {
-  console.log("Do IT...");
-}
+  function doIt() {
+    console.log("Do IT...");
+  }
 
-function inProcess() {
-  init();
-  sayHi();
-  doIt();
-}
+  function inProcess() {
+    init();
+    sayHi();
+    doIt();
+  }
 
-// inProcess();
+  // inProcess();
 
-// callback hell (example)
+  // callback hell (example)
 
-// recipe();
+  // recipe();
 
-function recipe() {
-  // imagine, we receives data from server
-  setTimeout(() => {
-    const IDs = [122, 144, 44, 155, 200, 201];
-    console.dir(IDs);
+  function recipe() {
+    // imagine, we receives data from server
+    setTimeout(() => {
+      const IDs = [122, 144, 44, 155, 200, 201];
+      console.dir(IDs);
 
-    // tries to receives data again
-    setTimeout(
-      id => {
-        const recipe = {
-          title: "tomato pizza italyno",
-          cockName: "Domingo",
-          id: id[2]
-        };
-        console.log(`${recipe.cockName}^${recipe.id} cooked ${recipe.title}`);
+      // tries to receives data again
+      setTimeout(
+        id => {
+          const recipe = {
+            title: "tomato pizza italyno",
+            cockName: "Domingo",
+            id: id[2]
+          };
+          console.log(`${recipe.cockName}^${recipe.id} cooked ${recipe.title}`);
 
-        // tries to receives data again
-        setTimeout(
-          id => {
-            const recipe = {
-              title: "big testy burger",
-              cockName: "Djoan",
-              id: id[4]
-            };
+          // tries to receives data again
+          setTimeout(
+            id => {
+              const recipe = {
+                title: "big testy burger",
+                cockName: "Djoan",
+                id: id[4]
+              };
 
-            console.log(
-              `${recipe.cockName}^${recipe.id} cooked ${recipe.title}`
-            );
+              console.log(
+                `${recipe.cockName}^${recipe.id} cooked ${recipe.title}`
+              );
 
-            // tries to receives data again
-            setTimeout(
-              id => {
-                const recipe = {
-                  title: "spaggety",
-                  cockName: "Bill",
-                  id: id[id.length - 1]
-                };
+              // tries to receives data again
+              setTimeout(
+                id => {
+                  const recipe = {
+                    title: "spaggety",
+                    cockName: "Bill",
+                    id: id[id.length - 1]
+                  };
 
-                console.log(
-                  `${recipe.cockName}^${recipe.id} cooked ${recipe.title}`
-                );
-
-                // update IDs
-                setTimeout(() => {
-                  const updIds = [1002, 456, 899, 900, 512];
-                  updIds.forEach(el => IDs.push(el));
-
-                  console.dir(IDs);
-                  console.log(`IDs was update`);
-
-                  // tries to receives data again
-                  setTimeout(
-                    id => {
-                      const recipe = {
-                        title: "mind of elephant",
-                        cockName: "Cocorubba",
-                        id: id[6]
-                      };
-
-                      console.log(
-                        `${recipe.cockName}^${recipe.id} cooked ${recipe.title}`
-                      );
-                    },
-                    1500,
-                    IDs
+                  console.log(
+                    `${recipe.cockName}^${recipe.id} cooked ${recipe.title}`
                   );
-                }, 1500);
-              },
-              1500,
-              IDs
-            );
-          },
-          1500,
-          IDs
-        );
-      },
-      1500,
-      IDs
-    );
-  }, 1500);
+
+                  // update IDs
+                  setTimeout(() => {
+                    const updIds = [1002, 456, 899, 900, 512];
+                    updIds.forEach(el => IDs.push(el));
+
+                    console.dir(IDs);
+                    console.log(`IDs was update`);
+
+                    // tries to receives data again
+                    setTimeout(
+                      id => {
+                        const recipe = {
+                          title: "mind of elephant",
+                          cockName: "Cocorubba",
+                          id: id[6]
+                        };
+
+                        console.log(
+                          `${recipe.cockName}^${recipe.id} cooked ${
+                            recipe.title
+                          }`
+                        );
+                      },
+                      1500,
+                      IDs
+                    );
+                  }, 1500);
+                },
+                1500,
+                IDs
+              );
+            },
+            1500,
+            IDs
+          );
+        },
+        1500,
+        IDs
+      );
+    }, 1500);
+  }
 }
